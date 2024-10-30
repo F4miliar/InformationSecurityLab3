2d6a22f0774ccced2d7b0c64f25978a6a48ce3d3
Maxim Lyubimov <maxim.lubimow2014@yandex.ru>
2024-10-30 22:45:57 +0400

    Добавьте файлы проекта.

---
+++ InformationSecurityLab3/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace InformationSecurityLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxUserPick = new System.Windows.Forms.ComboBox();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.buttonUserEdit = new System.Windows.Forms.Button();
+            this.listBoxObject = new System.Windows.Forms.ListBox();
+            this.textBoxObjectEdit = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxUserPick
+            // 
+            this.comboBoxUserPick.FormattingEnabled = true;
+            this.comboBoxUserPick.Location = new System.Drawing.Point(212, 11);
+            this.comboBoxUserPick.Name = "comboBoxUserPick";
+            this.comboBoxUserPick.Size = new System.Drawing.Size(135, 24);
+            this.comboBoxUserPick.TabIndex = 0;
+            this.comboBoxUserPick.SelectedIndexChanged += new System.EventHandler(this.comboBoxUserPick_SelectedIndexChanged);
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelUser.Location = new System.Drawing.Point(12, 9);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(158, 25);
+            this.labelUser.TabIndex = 1;
+            this.labelUser.Text = "Пользователь";
+            // 
+            // buttonUserEdit
+            // 
+            this.buttonUserEdit.Location = new System.Drawing.Point(415, 11);
+            this.buttonUserEdit.Name = "buttonUserEdit";
+            this.buttonUserEdit.Size = new System.Drawing.Size(225, 24);
+            this.buttonUserEdit.TabIndex = 2;
+            this.buttonUserEdit.Text = "Редактировать пользователя";
+            this.buttonUserEdit.UseVisualStyleBackColor = true;
+            // 
+            // listBoxObject
+            // 
+            this.listBoxObject.FormattingEnabled = true;
+            this.listBoxObject.ItemHeight = 16;
+            this.listBoxObject.Location = new System.Drawing.Point(17, 48);
+            this.listBoxObject.Name = "listBoxObject";
+            this.listBoxObject.Size = new System.Drawing.Size(330, 260);
+            this.listBoxObject.TabIndex = 3;
+            this.listBoxObject.SelectedIndexChanged += new System.EventHandler(this.listBoxObject_SelectedIndexChanged);
+            // 
+            // textBoxObjectEdit
+            // 
+            this.textBoxObjectEdit.Location = new System.Drawing.Point(355, 48);
+            this.textBoxObjectEdit.Multiline = true;
+            this.textBoxObjectEdit.Name = "textBoxObjectEdit";
+            this.textBoxObjectEdit.Size = new System.Drawing.Size(330, 260);
+            this.textBoxObjectEdit.TabIndex = 4;
+            this.textBoxObjectEdit.TextChanged += new System.EventHandler(this.textBoxObjectEdit_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 322);
+            this.Controls.Add(this.textBoxObjectEdit);
+            this.Controls.Add(this.listBoxObject);
+            this.Controls.Add(this.buttonUserEdit);
+            this.Controls.Add(this.labelUser);
+            this.Controls.Add(this.comboBoxUserPick);
+            this.Name = "Form1";
+            this.Text = "Матрица доступа (Выбор)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxUserPick;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Button buttonUserEdit;
+        private System.Windows.Forms.ListBox listBoxObject;
+        private System.Windows.Forms.TextBox textBoxObjectEdit;
+    }
+}
+
---
+++ InformationSecurityLab3/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InformationSecurityLab3
+{
+    public partial class Form1 : Form
+    {
+        List<string> users;
+        List<Text> objects;
+        List<List<int>> accesses;
+
+        int PickedUser;
+        int PickedObject = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MatrixInit();
+            UIElementsUpdate();
+        }
+
+        private void MatrixInit()
+        {
+            users = new List<string> { "Администратор", "Пользователь", "Читатель", "Редактор" };
+            objects = new List<Text> {
+                new Text("Объект1", "Текст первого объекта"),
+                new Text("Объект2", "Текст второго объекта"),
+                new Text("Объект3", "Текст третьего объекта") };
+            accesses = new List<List<int>> { 
+                new List<int> { 1, 0, 1, 2 },
+                new List<int> { 0, 0, 0, 0 },
+                new List<int> { 0, 1, 1, 1 },
+                new List<int> { 0, 2, 2, 2 },
+            };
+        }
+
+        private void UIElementsUpdate()
+        {
+            foreach (string user in users)
+                comboBoxUserPick.Items.Add(user);
+            foreach (Text text in objects)
+                listBoxObject.Items.Add(text.GetName());
+            comboBoxUserPick.SelectedIndex = 0;
+        }
+
+        private void UpdateText()
+        {
+            if (accesses[PickedUser][PickedObject + 1] == 0)
+            {
+                textBoxObjectEdit.Text = "";
+            } else if (accesses[PickedUser][PickedObject + 1] == 1)
+            {
+                textBoxObjectEdit.Text = objects[PickedObject].GetText();
+                textBoxObjectEdit.Enabled = false;
+            } else if (accesses[PickedUser][PickedObject + 1] == 2)
+            {
+                textBoxObjectEdit.Text = objects[PickedObject].GetText();
+                textBoxObjectEdit.Enabled = true;
+            }
+        }
+
+        private void comboBoxUserPick_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PickedUser = comboBoxUserPick.SelectedIndex;
+            if (PickedObject != -1)
+                UpdateText();
+        }
+
+        private void listBoxObject_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PickedObject = listBoxObject.SelectedIndex;
+            UpdateText();
+        }
+
+        private void textBoxObjectEdit_TextChanged(object sender, EventArgs e)
+        {
+            objects[PickedObject].SetText(textBoxObjectEdit.Text);
+        }
+    }
+
+    public class Text
+    {
+        private string Name;
+        private string Value;
+
+        public Text(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string GetName() => Name;
+        public string GetText() => Value;
+        public void SetText(string text) { Value = text; }
+    }
+}
